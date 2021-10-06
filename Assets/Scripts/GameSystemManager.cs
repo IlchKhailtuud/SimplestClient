ded3234e30f60eae0a86ed716170f614c0f766ea
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-10-05 23:23:39 -0400

    Created GameSystemManager script

--- Assets/Scripts/GameSystemManager.cs
+++ Assets/Scripts/GameSystemManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameSysyemManager : MonoBehaviour
+public class GameSystemManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
